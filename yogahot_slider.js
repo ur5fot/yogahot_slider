34674baf9ff0097f4245b8242ea17ebbd9fb944e
ur5fot <wagvpered@gmail.com>
2015-11-26 11:39:47 +0200

    убрано выделение, добалено: поддержка touch и пересщелкивание по клику

--- yogahot_slider.js
+++ yogahot_slider.js
@@ -6,32 +6,58 @@ function yogahotSlider() {
     var sliderContainer = slider.querySelector('.slider-content');
     var sliderContent = sliderContainer.querySelectorAll('div');
 
-    //for (var i = 1; i < sliderContent.length; i++) {
-    //    sliderContainer.removeChild(sliderContent[i]);
-    //}
-
     sliderContainer.innerHTML = sliderContent[0].innerHTML;
+    var mouseX =0;
 
-    thumb.onmousedown = function (e) {
+    thumb.onmousedown = function () {
         var thumbC = parseFloat(getComputedStyle(thumb).left);
         var thumbX = thumb.getBoundingClientRect().left;
         var step = Math.round((sliderLine.offsetWidth - thumb.offsetWidth ) / (sliderContent.length -1));
-        var mouseX =0;
 
         document.onmousemove = function (e) {
             var min = Math.min((e.pageX - thumbX + thumbC), sliderLine.offsetWidth - thumb.offsetWidth);
              mouseX = Math.round(Math.max(min, 0));
-            console.log(Math.round(mouseX / step) * step);
             thumb.style.left = Math.round(mouseX / step) * step + 'px';
             sliderContainer.innerHTML = sliderContent[Math.round(mouseX / step)].innerHTML;
+            if (window.getSelection) {
+                window.getSelection().removeAllRanges();
+            } else {
+                document.selection.empty();
+            }
         };
 
         document.onmouseup = function () {
             document.onmousemove = null;
             document.onmouseup = null;
         }
-    }
+    };
+    sliderLine.addEventListener('click', function(e){
+        var thumbC = parseFloat(getComputedStyle(thumb).left);
+        var thumbX = thumb.getBoundingClientRect().left;
+        var step = Math.round((sliderLine.offsetWidth - thumb.offsetWidth ) / (sliderContent.length -1));
+        var min = Math.min((e.pageX - thumbX + thumbC), sliderLine.offsetWidth - thumb.offsetWidth);
+        mouseX = Math.round(Math.max(min, 0));
+        thumb.style.left = Math.round(mouseX / step) * step + 'px';
+        sliderContainer.innerHTML = sliderContent[Math.round(mouseX / step)].innerHTML;
 
+    });
+    thumb.addEventListener('touchstart',
+        function touchstart() {
+            var thumbC = parseFloat(getComputedStyle(thumb).left);
+            var thumbX = thumb.getBoundingClientRect().left;
+            var step = Math.round((sliderLine.offsetWidth - thumb.offsetWidth ) / (sliderContent.length - 1));
+            var mouseX = 0;
+            document.addEventListener('touchmove',
+                function touchmove(event) {
+                    var e = event.targetTouches[0];
+                    var min = Math.min((e.pageX - thumbX + thumbC), sliderLine.offsetWidth - thumb.offsetWidth);
+                    mouseX = Math.round(Math.max(min, 0));
+                    thumb.style.left = Math.round(mouseX / step) * step + 'px';
+                    sliderContainer.innerHTML = sliderContent[Math.round(mouseX / step)].innerHTML;
+                }
+            , false);
+        }, false)
 }
+
 
 
